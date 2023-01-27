eac8ec792ffaf195135ae9cda937ac2ddb000f44
NRM129 <i.naru129@yahoo.ne.jp>
2023-01-27 09:42:47 +0900

    実装確認

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,10 +34,10 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    # prototype.update(prototype_params)  
-    if prototype.update(prototype_params)
-      redirect_to prototype_path(params[:id])
+    @prototype = Prototype.find(params[:id])
+    # @prototype.update(prototype_params)  
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
     else
       render :edit
     end
